fd47830fb2cdd7ed570b86ed1f35d1ba1e0a63a5
Suyash-programmer <suyashshell@gamil.com>
2025-10-02 12:25:52 +0530

    Updated layout: weather icon added

--- App_ver_0.2.py
+++ App_ver_0.2.py
@@ -1,13 +1,13 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import requests
+import requests, io
 
 root = Tk()
 root.geometry("480x320")
 root.title("Weather! Now 🌤️")
 
 #---- Load background Image ------
-bg_image = Image.open(r"D:\weather.jpg") 
+bg_image = Image.open(r"D:\pics\weather.jpg") 
 bg_photo = ImageTk.PhotoImage(image = bg_image)
 
 #----- creating canvas----
@@ -22,6 +22,8 @@ canvas.create_image(0,0,image= bg_photo,anchor= "nw")
 city_label = Label(root, text="Enter city name ",font=16,bg="lightblue")
 city_entry = Entry(root)
 
+icon_label = Label(root)
+icon_label.pack()
 button = Button(root,text="Get Weather")
 
 #--- result label (blank at first)
@@ -31,7 +33,8 @@ result_label = Label(root,text="",font=("Aerial",16),bg="lightblue")
 canvas.create_window(240,50,window=city_label) # x= 240 center horizontally
 canvas.create_window(240,100,window=city_entry) # entry box
 canvas.create_window(240,128,window=button)
-canvas.create_window(240,170,window=result_label)
+canvas.create_window(240,170,window= icon_label)
+canvas.create_window(240,220,window=result_label)
 
 API_key = "81803dda60468b87287a879ab670f0b5"
 
@@ -45,10 +48,25 @@ def get_weather(city):
 
         data = response.json()
 
+        #fetching the icon 
+        icon_code = data['weather'][0]['icon']  # fetching the icon code
+        icon_url = f"http://openweathermap.org/img/wn/{icon_code}@2x.png" # creating a url to download image
+
+        icon_data = requests.get(icon_url).content # downloada the image as raw bytes using "content"
+        img = Image.open(io.BytesIO(icon_data)) # tricking the pillow into treating our icon_data as if they were a file  in system
+        img = img.resize((40,40))
+        photo = ImageTk.PhotoImage(img)
+    
+        icon_label.config(image = photo)
+
+        icon_label.image = photo
+
+
         if response.status_code == 200:
             temp = data['main']['temp']
             desc = data['weather'][0]['description']
             city_name = data['name']
+
 
             result_label.config(text=f"{city_name}\n{temp}°C,{desc}")
 
